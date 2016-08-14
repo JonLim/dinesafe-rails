8b27b4f7652fbbe5a26d024f9a9979167ec9f31e
JonLim <jonlimtoronto@gmail.com>
2016-08-13 22:02:49 -0400

    First pass at using includes to make less calls to the db, seems to have made it faster.

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -2,21 +2,19 @@ class EstablishmentsController < ApplicationController
   before_action :set_establishment, only: [:show, :edit, :update, :destroy]
 
   def index
-    @establishments = Establishment.order(:name).page(params[:page]).per(30)
-
     if params[:search]
-      @establishments = Establishment.search(params[:search]).order(:name).page(params[:page]).per(30)
+      @establishments = Establishment.search(params[:search]).order(:name).page(params[:page]).per(30).includes(:inspections => :establishment)
     else
-      @establishments = Establishment.order(:name).page(params[:page]).per(30)
+      @establishments = Establishment.order(:name).page(params[:page]).per(30).includes(:inspections => :establishment)
     end
 
     if params[:sort] && params[:direction]
-      @establishments = Establishment.joins(:inspections).order(params[:sort] + ' ' + params[:direction]).page(params[:page]).per(30)
+      @establishments = Establishment.joins(:inspections).order(params[:sort] + ' ' + params[:direction]).page(params[:page]).per(30).includes(:inspections => :establishment)
     end
   end
 
   def show
-    @inspections = @establishment.inspections.order('date DESC')
+    @inspections = @establishment.inspections.order('date DESC').includes(:infractions => :inspection)
   end
 
   private
--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -2,10 +2,6 @@ class Establishment < ApplicationRecord
   has_many :inspections
   has_many :infractions, through: :inspections
 
-  def last_date
-    inspections.order('date DESC').first.date
-  end
-
   def last_status
     # Returns the latest inspection's status attribute
     inspections.order('date DESC').first.status
